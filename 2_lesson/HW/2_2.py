81f0f69dfdfe2365e2f37c03d2f4ae01966c83a6
Your Name <edidas@inbox.ru>
2022-12-21 22:38:26 +0200

    Add files

--- 2_lesson/HW/2_2.py
+++ 2_lesson/HW/2_2.py
@@ -1,7 +1,7 @@
 # Напишите программу, которая принимает на вход число N
 # и выдает набор произведений чисел от 1 до N в виде списка.
 
-print('Enter an integer: ')
+print('Enter an integer: ', end='')
 num = int(input())
 number_list = []
 f = 1
@@ -10,7 +10,7 @@ if num < 0:
 elif num == 0:
     print('The factorial of 0 is 1')
 else:
-    for i in range(1, num + 1):
+    for i in range(2, num + 1):
         f = f * i
         number_list.append(f)
 print(number_list)
