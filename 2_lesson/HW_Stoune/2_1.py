e2c5509b716f661e4b3572c614189774a9891f8d
Your Name <edidas@inbox.ru>
2022-12-25 17:15:29 +0200

    Add new file

---
+++ 2_lesson/HW_Stoune/2_1.py
@@ -0,0 +1,16 @@
+# Без работы с методами строк.
+# in -> out
+# - 6782 -> 23
+# - 0.67 -> 13
+# - 198.45 -> 27
+
+number = float(input('Введите вещественное число: '))
+num = len(str(number))
+number = number * 10 ** (num - 4)
+sum_num = 0
+for i in str(number):
+    if i != ".":
+        sum_num += int(i)
+
+print(number)
+print(sum_num)
