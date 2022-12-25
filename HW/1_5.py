f258ec38438818ec2189889bcac39ee441fadba4
Your Name <edidas@inbox.ru>
2022-12-25 15:02:15 +0200

    Change files

--- HW/1_5.py
+++ HW/1_5.py
@@ -9,6 +9,5 @@ print("Введите координаты точки B:")
 xB = int(input())
 yB = int(input())
 
-# distance = math.sqrt(pow(xB - xA, 2) + pow(yB - yA, 2))
 distance = sqrt((xB - xA)**2 + (yB - yA)**2)
 print(distance)
