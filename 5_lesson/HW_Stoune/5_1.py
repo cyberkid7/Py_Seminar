c98e27f6cdefb8321712780b1b5c9d4eae09d1c6
Your Name <edidas@inbox.ru>
2023-01-19 21:23:51 +0200

    Add new files in direct HW_Stoune

---
+++ 5_lesson/HW_Stoune/5_1.py
@@ -0,0 +1,59 @@
+# Задача 1:
+# Создайте программу для игры с конфетами человек против человека.
+# Условие задачи: На столе лежит заданное количество конфета.
+# Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой.
+# За один ход можно забрать не более чем 28 конфет.
+# Все конфеты оппонента достаются сделавшему последний ход.
+# Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+
+from random import randint
+
+
+def input_data(name):
+    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+    while x < 1 or x > 28:
+        x = int(input(f"{name}, введите корректное количество конфет: "))
+    return x
+
+
+def p_print(name, x, counter, values):
+    print(f"Ходил {name}, он взял {x}, теперь у него {counter}. Осталось на столе {values} конфет.")
+
+
+def bot_calc(values):
+    y = randint(1, 29)
+    while values-y <= 28 and values > 29:
+        y = randint(1, 29)
+    return y
+
+
+player1 = input("Введите имя первого игрока: ")
+player2 = "Bot"
+value = int(input("Введите количество конфет на столе: "))
+flag = randint(0, 2)  # флаг очередности
+if flag:
+    print(f"Первый ходит {player1}")
+else:
+    print(f"Первый ходит {player2}")
+
+counter1 = 0
+counter2 = 0
+
+while value > 28:
+    if flag:
+        k = input_data(player1)
+        counter1 += k
+        value -= k
+        flag = False
+        p_print(player1, k, counter1, value)
+    else:
+        k = bot_calc(value)
+        counter2 += k
+        value -= k
+        flag = True
+        p_print(player2, k, counter2, value)
+
+if flag:
+    print(f"Выиграл {player1}")
+else:
+    print(f"Выиграл {player2}")
